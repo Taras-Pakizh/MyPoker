69c3e623697c496d862d0d3fd1eac2756162b851
Taras Pakizh <pakizht@gmail.com>
2018-08-12 13:17:03 +0300

    hand ranking test

---
+++ HandRankTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyPoker.Logic;
+
+namespace MyPokerTest
+{
+    [TestClass]
+    public class HandRankTest
+    {
+        [TestMethod]
+        public void FlushStraightTest()
+        {
+            //arrange
+            Card[] hand = new Card[7]
+            {
+                new Card(CardSuit.Club, CardType.King),
+                new Card(CardSuit.Club, CardType.Nine),
+                new Card(CardSuit.Club, CardType.Ten),
+                new Card(CardSuit.Club, CardType.Jack),
+                new Card(CardSuit.Club, CardType.Two),
+                new Card(CardSuit.Heart, CardType.Two),
+                new Card(CardSuit.Club, CardType.Queen)
+            };
+            Card expectedHighCard = new Card(CardSuit.Club, CardType.King);
+            Combination expectedCombination = Combination.Straight_flush;
+
+            //act
+            var result = new HandRank().Parse_RoyalStraightFlush_Flush(hand);
+            Combination actualCombination = result.Item1;
+            Card actualHighCard = result.Item2;
+
+            //assert
+            Assert.AreEqual(expectedCombination, actualCombination);
+            Assert.AreEqual(expectedHighCard.suit, actualHighCard.suit);
+            Assert.AreEqual(expectedHighCard.type, actualHighCard.type);
+        }
+    }
+}
